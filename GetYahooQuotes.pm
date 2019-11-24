3b460ee3147fef9c6998f3c25679bc86101e0b3e
Joachim Naulet <jnaulet@rdinnovation.fr>
2019-11-24 15:03:47 +0100

    Fixed race condition on cookie jar if multiple dl

--- GetYahooQuotes.pm
+++ GetYahooQuotes.pm
@@ -7,20 +7,30 @@ our @EXPORT = qw(&get_yahoo_quotes);
 use strict;
 use warnings;
 
+use String::Random;
 use WWW::Curl::Easy;
+use POSIX qw(strftime);
 
 my $crumb = undef;
-my $cookiejar = "/tmp/cookiejar";
+my $cookiejar = undef;
+
+# Create cookiejar
+sub get_cookiejar
+{
+    my $str = String::Random->new;
+    return "/tmp/cookiejar_".strftime("%F@%T", localtime)."_".
+	$str->randregex("[A-Za-z0-9]{32}");
+}
 
 # Get the crumb value
-sub get_crumb {
+sub get_crumb
+{
     my ($sym) = @_;
     
     my $curl = WWW::Curl::Easy->new;
     my $url = "https://finance.yahoo.com/quote/$sym/?p=$sym";
     
     $curl->setopt(CURLOPT_URL, $url);
-    unlink($cookiejar);
     $curl->setopt(CURLOPT_COOKIEJAR, $cookiejar);
     $curl->setopt(CURLOPT_COOKIEFILE, $cookiejar);
     
@@ -28,20 +38,21 @@ sub get_crumb {
     my $data;
     $curl->setopt(CURLOPT_WRITEDATA, \$data);
     $curl->perform;
-
+    
     # Extract CrumbStore
-    (my $crumb) = $data =~ m/{"crumb":"([a-zA-Z0-9\.]*)"}/g;    
+    (my $crumb) = $data =~ m/{"crumb":"([a-zA-Z0-9\.]*)"}/g;
     return $crumb;
 }
 
-sub get_yahoo_quotes {
+sub get_yahoo_quotes
+{
     my ($sym, $filename) = @_;
-    # Try twice before giving up
-    $crumb = get_crumb($sym) unless defined $crumb;
-    $crumb = get_crumb($sym) unless defined $crumb;
 
+    $cookiejar = get_cookiejar unless defined $cookiejar;
+    $crumb = get_crumb($sym) unless defined $crumb;
+    
     if(!defined $crumb){
-	print "Unable to get crumb for $filename\n";
+	print STDERR "Unable to get crumb for $filename\n";
 	return -1;
     }
     
